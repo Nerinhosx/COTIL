a6e6ed4252af192237e0aec4e2b05610aaf5eae7
Nerinhosx <nerooliveira.arthur@gmail.com>
2024-02-15 22:42:51 -0300

    Conclusão do 1º exercício (16/2)

--- 2024/Desenvolvimento_de_Dispositivos_Moveis/2- Fevereiro/15/flutter_application_exercicio1/lib/ex1.dart
+++ 2024/Desenvolvimento_de_Dispositivos_Moveis/2- Fevereiro/15/flutter_application_exercicio1/lib/ex1.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
 
 import 'package:flutter/material.dart';
 
@@ -13,8 +13,88 @@ class Ex1 extends StatelessWidget {
           "Exercício 1",
           style: TextStyle(
             fontWeight: FontWeight.w700,
+            color: Colors.white,
           ),
         ),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 28, 101, 228),
+      ),
+
+      body: SingleChildScrollView(child:
+        Center(child:
+          Column(
+            children:[
+              SizedBox(height: 60,),
+              Text(
+                "Eu",
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.w500,
+                  backgroundColor: Colors.blueAccent,
+                ),
+              ),
+              
+              SizedBox(height: 60,),
+              Text(
+                "Amo",
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.w500,
+                  backgroundColor: Colors.red,
+                ),
+              ),
+
+              SizedBox(height: 60,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    "a",
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.w500,
+                      backgroundColor: Colors.yellow,
+                    ),
+                  ),
+
+                  Text(
+                    "aula",
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.w500,
+                      backgroundColor: Color.fromARGB(255, 163, 11, 0)
+                    ),
+                  ),
+
+                  Text(
+                    "da",
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.w500,
+                      backgroundColor: Color.fromARGB(255, 244, 216, 54),
+                    ),
+                  ),
+                ]  
+              ),
+
+              SizedBox(height: 60,),
+              Text(
+                "Tânia",
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.w500,
+                  backgroundColor: const Color.fromARGB(255, 206, 54, 244),
+                ),
+              ),
+
+              SizedBox(height: 50,),
+              SizedBox(child:
+                Image.network('https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/220px-Heart_coraz%C3%B3n.svg.png'),
+                height: 120,
+              ),
+            ]
+          ),
+        ),     
       ),
     );
   }

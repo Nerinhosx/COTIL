c66848f5343047039f885484ee10600b4ad504ed
Nerinhosx <nerooliveira.arthur@gmail.com>
2024-02-15 21:46:27 -0300

    Adição de imagens com uso de SizedBox (15/2)

--- 2024/Desenvolvimento_de_Dispositivos_Moveis/2- Fevereiro/15/flutter_application_exemplo1/lib/home.dart
+++ 2024/Desenvolvimento_de_Dispositivos_Moveis/2- Fevereiro/15/flutter_application_exemplo1/lib/home.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -18,11 +18,51 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.amberAccent.shade400,
       ),
       backgroundColor: Colors.amberAccent.shade100,
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 150,
+              child: Image.network('https://upload.wikimedia.org/wikipedia/commons/7/70/Apis_mellifera_-_Brassica_napus_-_Valingu.jpg'),
+            ),
+            Text(
+              "Apis mellifera",
+              style: TextStyle(
+                fontStyle: FontStyle.italic
+              ),
+            ),
+
+            SizedBox(
+              height: 150,
+              width: 200,
+              child: Image.network('https://upload.wikimedia.org/wikipedia/commons/0/0e/Abelha-jatai.jpg')
+            ),
+            Text(
+              "Tetragonisca angustula",
+              style: TextStyle(
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+
+            SizedBox(
+              height: 150,
+              width: 200,
+              child: Image.network('https://lh4.googleusercontent.com/-cC4xAvG_o5I/TWr2NiDHNcI/AAAAAAAAAIU/-hg-YF13tG4/s1600/Nova+Imagem.png'),
+            ),
+            Text(
+              "Melipona quadrifasciata anthidioides",
+              style: TextStyle(
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ]
+        ),
+      ),
       bottomNavigationBar: BottomAppBar(
         color: Colors.amber,
         height: 80,
         child: Text(
-          "Sejam bem-vindos!",
+          "Essa é a página principal do app",
           style: TextStyle(
             color: Colors.black87,
             fontSize: 30,

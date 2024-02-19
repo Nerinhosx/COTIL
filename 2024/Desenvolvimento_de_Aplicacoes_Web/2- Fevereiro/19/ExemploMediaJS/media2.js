7427786ca0737dc9f3fa23f663fedd681b94aab2
Nerinhosx <nerooliveira.arthur@gmail.com>
2024-02-19 08:52:52 -0300

    Criação de JS externo

---
+++ 2024/Desenvolvimento_de_Aplicacoes_Web/2- Fevereiro/19/ExemploMediaJS/media2.js
@@ -0,0 +1,32 @@
+function calcularMedia() {
+
+    var n1 = document.getElementById("nota1").value;
+    
+    var n2 = document.querySelector("#nota2").value;
+    
+    if ( (n1.trim() == "") || (n2.trim() == "") ) {
+    
+    document.querySelector("#erro").innerHTML =
+                                  "As duas notas são obrigatórias! <br><br>";
+       document.querySelector("#erro").style.color = "red";
+    
+    } else {
+    
+    document.querySelector("#erro").innerHTML = "";
+    
+    var media = ( parseFloat(n1) + parseFloat(n2) ) / 2;
+    
+    document.querySelector("#media").innerHTML =  "<b>Média:" + media.toFixed(1) + "</b>";
+    
+       document.querySelector("#situacao").style.color = "white";
+    if (media >= 6.0) {
+    document.querySelector("#situacao").textContent = "Situação:  APROVADO";
+    document.querySelector("#situacao").style.backgroundColor = "green";
+    } else {
+    document.querySelector("#situacao").textContent = "Situação: REPROVADO";
+    document.querySelector("#situacao").style.backgroundColor = "red";
+    }
+       
+        } //else
+    
+    } //function

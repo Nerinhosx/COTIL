367c947d863437599dad3e3d98bd37cf3f4cb708
Nerinhosx <nerooliveira.arthur@gmail.com>
2024-02-23 11:03:11 -0300

    Adição de exemplo com uso do método round() (DAW, 22/2)

---
+++ 2024/Desenvolvimento_de_Aplicacoes_Web/2- Fevereiro/23/exemplo1.php
@@ -0,0 +1,55 @@
+<?php
+
+    // "==" verifica se o conteúdo é igual, independente do tipo
+    // "===" verifica se o conteúdo e o tipo são iguais
+        $nota1 = 8;
+        $nota2 = 6;
+        $media = ($nota1 + $nota2)/2;
+        $media = round($media);
+
+        if ($media >=6){
+            $situacao = "Aprovado";
+            $formatacao = "ap";
+        }else if(($media < 6) && ($media >3)){
+            $situacao = "Dependencia";
+            $formatacao = "dp";
+        } else{
+            $situacao = "Reprovado";
+            $formatacao = "rep";
+        }
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Média</title>
+
+    <style type="text/css">
+        .ap{
+            color: white;
+            background-color: green;
+        }
+
+        .dp{
+            color: black;
+            background-color: yellow;
+        }
+
+        .rep{
+            color: white;
+            background-color: red;
+        }
+    </style>
+</head>
+<body>
+    <?= $media ?>
+
+    <hr>
+
+    <span class="<?= $formatacao ?>">
+        <?= $situacao ?>
+    </span>
+</body>
+</html>

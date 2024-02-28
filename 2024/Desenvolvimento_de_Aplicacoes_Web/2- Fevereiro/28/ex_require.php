e05e1352c3e41951ef0aa979bab5241211882ca4
Nerinhosx <nerooliveira.arthur@gmail.com>
2024-02-28 08:54:05 -0300

    Exemplos de chamada de arquivo externo (include/require) adicionados (DAW, 28/2)

---
+++ 2024/Desenvolvimento_de_Aplicacoes_Web/2- Fevereiro/28/ex_require.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt - br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testando Menu com Include</title>
+</head>
+<body>
+    <?php
+        require("menu.html");
+        //require_once("menu.html");  (mesma coisa que o de cima, mas impede que o arquivo seja incluído mais de uma vez)
+    ?>
+
+    <br><br>
+    Testando Menu com Include
+
+</body>
+</html>

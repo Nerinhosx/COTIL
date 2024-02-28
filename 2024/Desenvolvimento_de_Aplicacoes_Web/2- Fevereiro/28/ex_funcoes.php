7da68378b38efab4b4629d23f488ab2e93dec56a
Nerinhosx <nerooliveira.arthur@gmail.com>
2024-02-28 09:24:12 -0300

    Exemplo de função interna adicionado (DAW, 28/2)

---
+++ 2024/Desenvolvimento_de_Aplicacoes_Web/2- Fevereiro/28/ex_funcoes.php
@@ -0,0 +1,40 @@
+<?php
+    function calcMedia($n1, $n2){
+        $media = ($n1 + $n2)/2;
+        return $media;
+    }
+
+    $media = calcMedia(7.0, 9.0);
+
+    $msg = "Média = " . $media . "<br>";
+
+    if($media>=6){
+        $msg = $msg . "<br><span id='aprovado'>APROVADO!</span>";
+    } else{
+        $msg = $msg . "<br><span id='reprovado'>REPROVADO</span>";
+    }
+?>
+
+<html>
+    <head>
+        <title>Média</title>
+        
+        <style>
+            #reprovado{
+                background-color: red;
+                color: white;
+                font-weight: bold;
+            }
+
+            #aprovado{
+                background-color: green;
+                color: white;
+                font-weight: bold;
+            }
+        </style>
+    </head>
+
+    <body>
+        <?= $msg ?>
+    </body>
+</html>

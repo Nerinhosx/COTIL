bfcf5d452bfa278be712d6a2edd291bdf40db678
Nerinhosx <nerooliveira.arthur@gmail.com>
2024-03-25 08:38:12 -0300

    Ajustes nos exemplos de manipulação de string (TTI, 25/3)

--- 2024/Topicos_em_Tecnologia_da_Informação/3- Março/25/exempString.py
+++ 2024/Topicos_em_Tecnologia_da_Informação/3- Março/25/exempString.py
@@ -2,4 +2,4 @@ frase = str(input("Digite uma sequência: "))
 qCar = frase[3:7:2]#x : y : z ----> índice de "pulo" (de 2 em 2, de 3 em 3 etc)
 #                   |   L--> posição do valor final
 #                   L-->posição do valor inicial
-print(f"Os caracteres, do 4º ao 7º, são: {qCar}")
+print(f"Os caracteres, do 4º ao 7º (pulando de 2 em 2), são: {qCar}")
--- 2024/Topicos_em_Tecnologia_da_Informação/3- Março/25/exempString2.py
+++ 2024/Topicos_em_Tecnologia_da_Informação/3- Março/25/exempString2.py
@@ -2,4 +2,4 @@ frase = str(input("Digite uma sequência: "))
 qCar = frase[:7]#[:x] OU [x:] 
 #                  |      L--> posição inicial do corte (de x até o final do array ou string)
 #                  L--> posição de limite do corte (de 0 até x)
-print(f"Os caracteres, do 4º ao 7º, são: {qCar}")
+print(f"Os caracteres, do 1º ao 7º, são: {qCar}")

c4bfb1f2868a2b76b00d94e96f35ba5e28b9f549
Nerinhosx <nerooliveira.arthur@gmail.com>
2024-03-27 09:23:57 -0300

    Exemplo de redirect no login add (DAW, 27/3)

---
+++ 2024/Desenvolvimento_de_Aplicacoes_Web/3- Março/27/html_redirect/alunoRedirect.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aluno</title>
+</head>
+<body>
+    <a href="">Controle de faltas</a>
+    <a href="">Boletim</a>
+    <hr>
+
+    <h1>Aluno - login: <?=$_GET['login']?></h1>
+    <p>Bem-vindo <?=$_GET['login']?></p>
+    <h2>Recados</h2>
+    <p>Caros alunos, favor inscrever-se na Oficina de Artes até 28/03/2024</p>
+</body>
+</html>
---
+++ 2024/Desenvolvimento_de_Aplicacoes_Web/3- Março/27/html_redirect/professorRedirect.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Professor</title>
+</head>
+<body>
+    <a href="">Controle de Frequência</a>
+    <a href="">Controle de Notas</a>
+    <hr>
+
+    <h1>Professor - Login: <?=$_GET['login']?></h1>
+    <p>Bem-vindo Professor(a)</p>
+    <h2>Recados</h2>
+    <p>Caros professores, favor lançar as datas e pesos das avaliações no sistema Sophia até 31/03/2024</p>
+</body>
+</html>

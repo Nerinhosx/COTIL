0cc581974d70d1728e7036fd6625d6100d268d74
Nerinhosx <nerooliveira.arthur@gmail.com>
2024-05-10 11:10:06 -0300

    Exxercício de encriptação alterado (DAW, 10/5)

---
+++ 2024/Desenvolvimento_de_Aplicacoes_Web/5- Maio/08/form.php
@@ -0,0 +1,75 @@
+<!--localhost/COTIL/2024/Desenvolvimento_de_Aplicacoes_Web/5- Maio/08/form.php-->
+<?php
+    $msg = "";
+    $loginDef = "Nerson";
+    $passwDef = crypt("1234", "nerson");
+    if($_SERVER["REQUEST_METHOD"] === "POST"){
+        $login = $_POST["login"];
+        $passw = $_POST["passw"];
+
+        if(!isset($login) || !isset($passw)){
+            $msg = "Login e senha são obrigatórios!";
+        }
+        else if(strlen($login)<5){
+            $msg = "O login precisa ter 5 ou mais caracteres.";   
+        }
+        else if(strlen($passw)<8){
+            $msg = "A senha precisa ter 8 ou mais caracteres.";
+        }
+        else if($login==$loginDef && crypt($passw, "nerson")==$passwDef){
+            header("Location");
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login do Sistema</title>
+
+    <style>
+        section{
+            width: 100%;
+            height: 50vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        form{
+            font-size: 30px;
+        }
+
+        #btn{
+            width: 176px;
+            height: 30px;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1 style="text-align: center; font-size: 54px;">Informe suas credenciais</h1>
+        <hr>
+    </header>
+
+    <section>
+        <form method="post">
+            <label for="login">Login:</label><br>
+            <input type="text" name="login">
+
+            <br><br>
+
+            <label for="passw">Senha:</label><br>
+            <input type="password" name="passw">
+
+            <br><br>
+
+            <input type="button" id="btn" value="Logar">
+        </form>
+
+        <?=$msg?>
+    </section>
+</body>
+</html>
--- 2024/Desenvolvimento_de_Aplicacoes_Web/5- Maio/08/login.php
+++
@@ -1,5 +0,0 @@
-<!--localhost/COTIL/2024/Desenvolvimento_de_Aplicacoes_Web/5- Maio/08/login.php-->
-
-<?php
-    
-?>

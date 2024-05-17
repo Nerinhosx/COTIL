c953b73861a311febdb0e781644306610943523d
Nerinhosx <nerooliveira.arthur@gmail.com>
2024-05-17 11:08:15 -0300

    Finalização de exercício (DAW, 17/5)

--- 2024/Desenvolvimento_de_Aplicacoes_Web/5- Maio/08/outrapagina.php
+++ 2024/Desenvolvimento_de_Aplicacoes_Web/5- Maio/08/outrapagina.php
@@ -1,25 +1,20 @@
+<!--localhost/COTIL/2024/Desenvolvimento_de_Aplicacoes_Web/5- Maio/08/outrapagina.php-->
 <?php
     session_start();
+    if(!isset($_SESSION["login"])){
+        include_once("erro.html");
+    }else{
+        include_once("outrapagina.html");
+    }
 ?>
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Outra página</title>
 
-    <style>
-        body{
-            text-align: center;
-            font-size: 46px;
-        }
-    </style>
 </head>
 <body>
-    Login: <?=$_SESSION["login"]?>
-    <br><br>
-    Senha: <?=$_SESSION["password"]?>
-    <br><br>
-    Deu bom 👌
 </body>
 </html>

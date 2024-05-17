cf6a634b505bff2d67712cae68dd36786b160ef3
Nerinhosx <nerooliveira.arthur@gmail.com>
2024-05-17 10:02:54 -0300

    Exercício de navigation iniciado (DDM, 17/5)

---
+++ 2024/Desenvolvimento_para_Dispositivos_Moveis/5- Maio/17/flutter_application_exercicio_navigation/lib/vava.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
+
+import 'package:flutter/material.dart';
+
+class MyVava extends StatefulWidget {
+  const MyVava({super.key});
+
+  @override
+  State<MyVava> createState() => _MyVavaState();
+}
+
+class _MyVavaState extends State<MyVava> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Valorinfo", style: TextStyle(fontFamily: "VavaFont", fontSize: 46)),
+        centerTitle: true,
+        backgroundColor: Color.fromRGBO(255, 70, 85, 1),
+      ),
+
+      
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/vavaFundo.png"),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Column(
+          children: [
+            Image.asset("images/vavaLogo.png"),
+          ],
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        ),
+      )
+    );
+  }
+}

55ea09521e9f656f392b6913dc55e25bc4a6caf0
Nerinhosx <nerooliveira.arthur@gmail.com>
2024-06-14 11:09:50 -0300

    Inicialização de atividade prática  (DAW, 14/6)

---
+++ 2024/Desenvolvimento_de_Aplicacoes_Web/6- Junho/14/form.php
@@ -0,0 +1,38 @@
+<?php
+    if($_SERVER["REQUEST_METHOD"] === "GET"){
+
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário</title>
+
+    <style>
+        h1{
+            text-align: center;
+            font-size: 48px;
+        }
+
+        section{
+            text-align: center;
+            font-size: 24px;
+        }
+
+        body{
+            width: 98vw;
+            height: 98vh;
+            display: flex;
+            flex-direction: column;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Busca de alunos por turma</h1>
+        <section>Informe a turma desejada</section>
+    </header>
+</body>
+</html>

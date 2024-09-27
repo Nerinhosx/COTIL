60ca523ef2a0952711693a86346066a64b2a4859
Nerinhosx <nerooliveira.arthur@gmail.com>
2024-09-27 09:01:18 -0300

    Adição de métodos-exemplo (DDM, 27/9)

--- 2024/Desenvolvimento_para_Dispositivos_Moveis/9- Setembro/27/apiTesteConsumoFlutter1/src/main/java/com/example/apiTesteConsumoFlutter1/controller/AlunoFlutterController.java
+++ 2024/Desenvolvimento_para_Dispositivos_Moveis/9- Setembro/27/apiTesteConsumoFlutter1/src/main/java/com/example/apiTesteConsumoFlutter1/controller/AlunoFlutterController.java
@@ -1,4 +1,23 @@
 package com.example.apiTesteConsumoFlutter1.controller;
 
+import com.example.apiTesteConsumoFlutter1.model.AlunoFlutter;
+import com.example.apiTesteConsumoFlutter1.repository.AlunoFlutterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping ("/apiAlunoFlutter")
 public class AlunoFlutterController {
+
+    @Autowired
+    AlunoFlutterRepository alFlRepo;
+
+    @GetMapping("/todos")
+    public List<AlunoFlutter> buscarTodosAlunos(){
+        return alFlRepo.findAll();
+    }
 }

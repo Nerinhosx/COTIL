a4a3df8dbb0b3d57229fb4839b36a0d835df54e5
Nerinhosx <nerooliveira.arthur@gmail.com>
2024-09-27 08:08:08 -0300

    Adição de estrutura base para API (DDM, 27/9)

---
+++ 2024/Desenvolvimento_para_Dispositivos_Moveis/9- Setembro/27/apiTesteConsumoFlutter1/src/main/java/com/example/apiTesteConsumoFlutter1/ApiTesteConsumoFlutter1Application.java
@@ -0,0 +1,13 @@
+package com.example.apiTesteConsumoFlutter1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTesteConsumoFlutter1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTesteConsumoFlutter1Application.class, args);
+	}
+
+}
---
+++ 2024/Desenvolvimento_para_Dispositivos_Moveis/9- Setembro/27/apiTesteConsumoFlutter1/src/test/java/com/example/apiTesteConsumoFlutter1/ApiTesteConsumoFlutter1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiTesteConsumoFlutter1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTesteConsumoFlutter1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

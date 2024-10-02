2197813bf003744eeee9daedcf67346d2cabf722
Nerinhosx <nerooliveira.arthur@gmail.com>
2024-10-02 08:58:36 -0300

    Finalização da exclusão de imagem (DAW, 2/10)

--- 2024/Desenvolvimento_de_Aplicacoes_Web/8 - Agosto/16/exclusao.php
+++ 2024/Desenvolvimento_de_Aplicacoes_Web/8 - Agosto/16/exclusao.php
@@ -21,11 +21,25 @@
 
         include('conexaoBD.php');
         if((isset($ra)) && ($ra!="")){
-            $stmt = $pdo->prepare("delete from alunos where ra = :ra");
-            $stmt->bindParam(":ra", $ra);
-
             try{
+                //Pegando o caminho local em que o arquivo está salvo
+                $stmt = $pdo->prepare("select arquivoFoto from alunos where ra = :ra");
+                $stmt->bindParam(":ra", $ra);
                 $stmt->execute();
+                $row = $stmt->fetch();
+                $arquivoFoto = $row['arquivoFoto'];
+
+                //Removendo o aluno do BD
+                $stmt = $pdo->prepare("delete from alunos where ra = :ra");
+                $stmt->bindParam(":ra", $ra);
+                $stmt->execute();
+
+                //Removendo o arquivo da pasta local
+                if($arquivoFoto != null){
+                    unlink($arquivoFoto);
+                }
+
+                echo "<h3>Aluno excluído com sucesso!</h3>";
             }
             catch(PDOException $e){
                 echo "Erro:" . $e->getMessage();

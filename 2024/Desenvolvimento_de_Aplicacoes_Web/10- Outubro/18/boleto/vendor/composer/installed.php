3cf4ddcd7c2ab24cc16c1550416690e880b8b3b8
Jerso06 <jedsonhenrique2006@gmail.com>
2024-10-18 11:06:48 -0300

    Exemplo de pacote obtido com Composer (DAW, 18/10)

---
+++ 2024/Desenvolvimento_de_Aplicacoes_Web/10- Outubro/18/boleto/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'openboleto/openboleto' => array(
+            'pretty_version' => 'v1.0',
+            'version' => '1.0.0.0',
+            'reference' => 'ae70c2502b12cfbdad28377cddd8014031de76ce',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../openboleto/openboleto',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

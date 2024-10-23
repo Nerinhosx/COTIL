652cbd67cf8daa227df5d8a73707dd460b99a201
Jerso06 <jedsonhenrique2006@gmail.com>
2024-10-23 08:55:48 -0300

    Exemplo de pacote vindo do composer (DAW, 23/10)

---
+++ 2024/Desenvolvimento_de_Aplicacoes_Web/10- Outubro/23/boleto/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4431b0ffe268abed27adf5d59fd9d5a6c72680c4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4431b0ffe268abed27adf5d59fd9d5a6c72680c4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'openboleto/openboleto' => array(
+            'pretty_version' => 'v1.0',
+            'version' => '1.0.0.0',
+            'reference' => 'ae70c2502b12cfbdad28377cddd8014031de76ce',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../openboleto/openboleto',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
